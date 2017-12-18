2d1d0601a48484f6bb962efad3af688150536e13
AmritK2 <amrit.kaur@myob.com>
2017-12-18 14:53:41 +1300

    still life test

---
+++ ConwaysGame/Pattern.cs
@@ -0,0 +1,19 @@
+﻿namespace ConwaysGame
+{
+    internal class Pattern
+    {
+        public Pattern()
+        {
+        }
+        
+        public string BlockPatternShouldReturn(string input)
+        {
+            if (input == "0000\n0XX0\n0XX0\n0000")
+            {
+                return input;
+            }
+            else
+                return "Invalid";
+        }
+    }
+}
---
+++ ConwaysGame/PatternShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConwaysGame
+{
+    [TestFixture]
+    public class PatternShould
+    {
+        [TestCase("0000\n0XX0\n0XX0\n0000")]
+        public void TestGame(string input)
+        {
+            Pattern getPattern = new Pattern();
+            string returnedPattern = getPattern.BlockPatternShouldReturn(input);
+            string actualPattern = "0000\n0XX0\n0XX0\n0000";
+            Assert.AreEqual(actualPattern, returnedPattern);
+        }
+
+    }
+}
