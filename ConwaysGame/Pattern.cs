de45f7f0dc480722eff81459ba3ae86c37a76b47
AmritK2 <amrit.kaur@myob.com>
2017-12-20 14:18:44 +1300

    the max/min functionality not working yet

--- ConwaysGame/Pattern.cs
+++ ConwaysGame/Pattern.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using NUnit.Framework.Api;
 
@@ -111,7 +112,17 @@ namespace ConwaysGame
             var r = 0;
             var c = 0;
             var output = "";
-            
+
+//            string[] array = input.Split('\n');
+//            for (int i = 0; i < array.Length; i++)
+//            {
+//                if (array[i] == "X")
+//                {
+//                    CheckNeighboursWithMinMax(array, i);
+//                }
+//            }
+//           
+
             for (int i = 0; i < input.Length; i++)
             {
                 for (int j = 0; j < input.Length; j++)
@@ -123,13 +134,13 @@ namespace ConwaysGame
                     }
                     else if (input[j] == 'X')
                     {
-                        var result = CheckNeighbours(input, r, c);
+                        var result = CheckNeighboursWithMinMax(input, r, c);
+                            
                         if (result < 2)
                         {
                             char[] ch = input.ToCharArray();
                             ch[j] = '0';
                             output = new string(ch);
-                            
                         }
                         else if (result == 2 || result <= 3)
                         {
@@ -137,7 +148,6 @@ namespace ConwaysGame
                             ch[j] = 'X';
                             output = new string(ch);
                         }
-                        
                     }
                     c++;
                 }
@@ -145,6 +155,66 @@ namespace ConwaysGame
             return output;
         }
 
+        private int CheckNeighboursWithMinMax(string input, int x, int y)
+        {
+            int totalColumns = 0;
+            int totalRows = 0;
+            int count = 0;
+            for (int b = 0; b < input.Length; b++)
+            {
+                
+                if (input[b] == '\n')
+                {
+                    totalColumns = -1;
+                    totalRows++;
+                }
+                totalColumns++;
+            }
+            
+            int minX = Math.Max(0, x - 1);
+            int maxX = Math.Min(x + 1, totalRows);
+            int minY = Math.Max(0, y - 1);
+            int maxY = Math.Min(y + 1, totalColumns);
 
+            if (input[(minX - 1)] == 'X' && input[minY - 1] == 'X')
+            {
+                count++;
+            }
+            else if (input[minX] == 'X')
+            {
+                count++;
+            }
+            else if (input[(maxY - 1)] == 'X' && input[minX + 1] == 'X')
+            {
+                count++;
+            }
+
+            else if (input[maxY] == 'X')
+            {
+                count++;
+            }
+            else if (input[(maxX + 1)] == 'X' && input[maxY + 1] == 'X')
+            {
+                count++;
+            }
+            else if (input[maxX] == 'X')
+            {
+                count++;
+            }
+            else if (input[(maxX - 1)] == 'X' && input[minY + 1] == 'X')
+            {
+                count++;
+            }
+            else if (input[minY] == 'X')
+            {
+                count++;
+            }
+       
+            return count;
+        }
+
+
+
+        
     }
 }
