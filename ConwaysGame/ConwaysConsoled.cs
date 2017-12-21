b37b553481bcd46f6f3458fbf7d2c6884d388b17
AmritK2 <amrit.kaur@myob.com>
2017-12-22 11:30:00 +1300

    finished game

--- ConwaysGame/ConwaysConsoled.cs
+++ ConwaysGame/ConwaysConsoled.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConwaysGame
@@ -10,23 +11,36 @@ namespace ConwaysGame
     {
         static void Main(string[] args)
         {
-            string input = "*****\n" +
-                           "**11*\n" +
-                           "*11**\n" +
-                           "**1**\n" +
-                           "*****";
+            string input = "***************\n" +
+                           "***************\n" +
+                           "***************\n" +
+                           "***************\n" +
+                           "***************\n" + 
+                           "***************\n" +
+                           "*******11******\n" +
+                           "******11*******\n" +
+                           "*******1*******\n" +
+                           "***************\n" +
+                           "***************\n" +
+                           "***************\n" +
+                           "***************\n" +
+                           "***************\n" +
+                           "***************";
             while (true)
             {
                 Pattern pattern = new Pattern();
                 var result = pattern.TransformedCellString(input);
 
+
                 while (result != input)
                 {
                     Console.WriteLine(result);
-                    Console.WriteLine("---------");
                     input = result;
+ 
                 }
+                Thread.Sleep(1000);
 
+                Console.Clear();
             }
 
         }
